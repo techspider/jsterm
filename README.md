1ef3e0d76ae032d5080e7184b24028473c27de68
mr_chainman <rvreman7@me.com>
2019-02-28 23:01:29 +0400

    Add user input

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ What works
  * dynamic cursor
  * cursor blink
  * auto scroll
+ * user input
 
 What doesn't work
- * user input
  * clear screen function
+
+Bugs
+ * The keyboard input can be buggy
--- jsterm-dev.js
+++ jsterm-dev.js
@@ -1,6 +1,69 @@
 var _TGlobal = {
     createdTerms: 0,
-    terminalOpts: []
+    terminalOpts: [],
+    kbdListen: false,
+    kbdCb: null,
+    lastPrintElem: null,
+    inputString: "",
+    kbdSecret: 0,
+    kbdShiftHeld: false,
+    kbdCtrlHeld: false
+}
+
+function locateCharFromMap(isShiftKey, characterCode) {
+    if ( characterCode === 27 || characterCode === 8 || characterCode === 9 || characterCode === 20 || characterCode === 16 || characterCode === 17 || characterCode === 91 || characterCode === 13 || characterCode === 92 || characterCode === 18 ) {
+        return false;
+    }
+    if (typeof isShiftKey != "boolean" || typeof characterCode != "number") {
+        return false;
+    }
+    var characterMap = [];
+    characterMap[192] = "~";
+    characterMap[49] = "!";
+    characterMap[50] = "@";
+    characterMap[51] = "#";
+    characterMap[52] = "$";
+    characterMap[53] = "%";
+    characterMap[54] = "^";
+    characterMap[55] = "&";
+    characterMap[56] = "*";
+    characterMap[57] = "(";
+    characterMap[48] = ")";
+    characterMap[109] = "_";
+    characterMap[107] = "+";
+    characterMap[219] = "{";
+    characterMap[221] = "}";
+    characterMap[220] = "|";
+    characterMap[59] = ":";
+    characterMap[222] = "\"";
+    characterMap[188] = "<";
+    characterMap[190] = ">";
+    characterMap[191] = "?";
+    characterMap[32] = " ";
+    var character = "";
+    if (isShiftKey)
+    {
+        if ( characterCode >= 65 && characterCode <= 90 )
+        {
+            character = String.fromCharCode(characterCode);
+        } 
+        else 
+        {
+            character = characterMap[characterCode];
+        }
+    } 
+    else 
+    {
+        if ( characterCode >= 65 && characterCode <= 90 ) 
+        {
+            character = String.fromCharCode(characterCode).toLowerCase();
+        } 
+        else
+        {
+            character = String.fromCharCode(characterCode);
+        }
+    }
+    return character;
 }
 
 var Terminal = function(containerId, width, height)
@@ -26,6 +89,8 @@ var Terminal = function(containerId, width, height)
         this.textContainer.style.fontSize = this.fontSize;
         this.textContainer.style.fontFamily = this.fontFamily;
         this.textContainer.style.fontWeight = 900;
+        this.textContainer.style.overflowX = 'hidden';
+        this.textContainer.style.maxWidth = `${this.width}px`;
 
         this.container.style.backgroundColor = this.backColor;
         this.container.style.height = `${this.height}px`;
@@ -50,16 +115,75 @@ var Terminal = function(containerId, width, height)
         }
     }
 
-    this.print = function(text)
+    this.print = function(text, chrattr)
     {
         this.textContainer.removeChild(this.cursor);
         var textElement = document.createElement('span');
         textElement.innerText = text;
         textElement.style.whiteSpace = 'pre-line';
         textElement.style.display = 'inline';
+        if(chrattr) textElement.setAttribute(`is_kbd_char${_TGlobal.kbdSecret}`, "true");
+        _TGlobal.lastPrintElem = textElement;
         this.textContainer.appendChild(textElement);
         this.textContainer.appendChild(this.cursor);
         this.scrollUpdate();
+    }
+
+    this._keyboardListenerUp = function(event)
+    {
+        switch(event.keyCode)
+        {
+            case 16:
+                _TGlobal.kbdShiftHeld = false;
+                break;
+            case 17:
+                _TGlobal.kbdCtrlHeld = false;
+                break;
+        }
+    }
+
+    this._keyboardListener = function(event)
+    {//TODO set kbdlisten to false once done listening
+        _TGlobal.kbdListen = true;
+        switch(event.keyCode)
+        {
+            default:
+                var chr = locateCharFromMap(_TGlobal.kbdShiftHeld, event.keyCode);
+                _TGlobal.inputString += chr;
+                _TGlobal.t.print(chr, true);
+                break;
+            case 16: //SHIFT
+                _TGlobal.kbdShiftHeld = true;
+                break;
+            case 17: //CTRL
+                _TGlobal.kbdCtrlHeld = true;
+                break;
+            case 13: //RETURN, END THE LISTENING
+                _TGlobal.t.print('\n', true);
+                _TGlobal.kbdListen = false;
+                document.removeEventListener('keydown', _TGlobal.t._keyboardListener, true);
+                document.removeEventListener('keyup', _TGlobal.t._keyboardListenerUp, true);
+                _TGlobal.kbdCb(_TGlobal.inputString);
+                break;
+            case 8: //BKSP
+                if(!_TGlobal.t.textContainer.children.length > 2) return;
+                var lastCharEl = _TGlobal.t.textContainer.children[term.textContainer.children.length-2];
+                if((lastCharEl.getAttribute(`is_kbd_char${_TGlobal.kbdSecret}`) == null) || (lastCharEl.getAttribute(`is_kbd_char${_TGlobal.kbdSecret}`) != "true")) return; //probably not a keyboard character
+                _TGlobal.t.textContainer.removeChild(lastCharEl);
+                _TGlobal.inputString = _TGlobal.inputString.substring(0, _TGlobal.inputString.length - 1);
+                break;
+        }
+    }
+
+    this.input = function(cb)
+    {
+        if(_TGlobal.kbdListen) return;
+        _TGlobal.inputString = "";
+        _TGlobal.kbdCb = cb;
+        _TGlobal.t = this; //hook print function
+        _TGlobal.kbdSecret = Math.round(Math.random() * 1000);
+        document.addEventListener('keydown', this._keyboardListener, true);
+        document.addEventListener('keyup', this._keyboardListenerUp, true);
     }
 
     this.println = function(text)
